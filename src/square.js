d7b2bd561993dbc6654352f5861df04d302221d2
Bohdan P <bohdan.prymyak@izzi.software>
2021-10-30 01:29:36 +0300

    first solution, cant run test

--- src/square.js
+++ src/square.js
@@ -1,1 +1,7 @@
 'use strict';
+
+function square(a, b) {
+  return a ** b;
+}
+
+module.exports = { square };
--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const sum = require('./sum');
+const square = require('./square');
+
+function sumOfSquares(a, b) {
+  sum(square(a), square(b));
+}
+
+module.exports = { sumOfSquares };
