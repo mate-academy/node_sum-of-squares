db2194a04109df09af18aa2aa7e85a534156ae1f
IvanKolodiazhnyi <kysok.usera1337@gmail.com>
2022-01-27 00:52:50 +0200

    sum of squares

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,5 @@
 'use strict';
+
+exports.sum = (x, y) => {
+  return x + y;
+};
--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const { sum } = require('./sum');
+const { square } = require('./square');
+
+exports.sumOfSquares = (x, y) => {
+  const firstSquer = square(x);
+  const secondSquer = square(y);
+
+  return sum(firstSquer, secondSquer);
+};
