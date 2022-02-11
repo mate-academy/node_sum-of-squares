086584cc78d0e751470b24431c441deaebf4fc1a
Dmytro  Mykoliv <dmytro.mykoliv@indeema.com>
2022-02-11 16:20:11 +0200

    resolved

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const { sum } = require('./sum');
+const { square } = require('./square');
+
+function sumOfSquares(x, y) {
+  return sum(square(x), square(y));
+};
+
+// console.log(sumOfSquares(2, 5));
+
+module.exports = { sumOfSquares };
