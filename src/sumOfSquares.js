163ae123f9c73c6ec353553fbcae1b8cc4df447f
unknown <xpromt@gmail.com>
2022-05-04 17:47:17 +0300

    Result 00

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const { sum } = require('./sum.js');
+const { square } = require('./square.js');
+
+function sumOfSquares(x, y) {
+  return sum(square(x), square(y));
+}
+
+module.exports = { sumOfSquares };
