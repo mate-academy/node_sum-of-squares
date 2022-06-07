836dbf4f0dad6f07c16980912a0ed6f5cdb54d9a
Maksym Yaremenko <restartermord123@ukr.net>
2022-06-07 16:12:52 +0300

    Solution

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,13 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { sum } = require('./sum.js');
+const { square } = require('./square.js');
+
+function sumOfSquares(x, y) {
+  return sum(square(x), square(y));
+};
+
+// console.log(sumOfSquares(-7, -5));
+
+module.exports = { sumOfSquares };
