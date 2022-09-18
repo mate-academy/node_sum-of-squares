4efe77b666c97c322713ce9b51a46f0f897f9310
Pavlo <Pmoroz2004@gmail.com>
2022-09-18 11:07:18 +0300

    solution

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const sum = require('./sum').sum;
+const square = require('./square').square;
+
+module.exports = {
+  sumOfSquares(x, y) {
+    return sum(square(x), square(y));
+  },
+};
