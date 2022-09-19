2feec9e6fa25bc7ae46dc9a3d8415e77f8b46907
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-09-19 17:28:24 +0300

    solution

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const { sum } = require('./sum');
+const { square } = require('./square');
+
+// eslint-disable-next-line no-unused-vars
+function sumOfSquares(x, y) {
+  return sum(square(x), square(y));
+};
+
+module.exports = { sumOfSquares };
