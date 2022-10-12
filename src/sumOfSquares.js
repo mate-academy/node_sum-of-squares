c401e09196f3b4bb4820d3e759058e179c59982b
Anatolii Bukoros <bukoros.anatoliy@gmail.com>
2022-10-12 16:14:21 +0300

    added node modules

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const { square } = require('./square.js');
+const { sum } = require('./sum.js');
+
+function sumOfSquares(x, y) {
+  return sum(square(x), square(y));
+}
+
+module.exports.sumOfSquares = sumOfSquares;
