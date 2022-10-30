d2cf8c642e7b5aea61f992aba30ab02ca7150b5f
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-10-30 16:55:13 +0200

    develop

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const { square } = require('./square');
+const { sum } = require('./sum');
+
+const sumOfSquares = (a, b) => {
+  return sum(square(a), square(b));
+};
+
+module.exports = { sumOfSquares };
