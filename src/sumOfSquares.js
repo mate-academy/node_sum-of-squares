d7544eb9c7973a2734e4a1771f3e24a15eadc2eb
Roman Veremchuk <roman.veremchuk.ki.2020@lpnu.ua>
2022-11-17 11:20:44 +0200

    solution

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const sum = require('./sum');
+const square = require('./square');
+
+function sumOfSquares(x, y) {
+  return sum.sum(square.square(x), square.square(y));
+}
+
+module.exports = { sumOfSquares };
