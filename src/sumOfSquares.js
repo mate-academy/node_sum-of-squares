6102fc3f4c6d8975c0fe844adcb7faafffa6531e
Metin BICAKSIZ <metinbicaksiz@mail.com>
2023-05-29 14:14:40 +0300

    first attempt

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const sum = require('./sum');
+const square = require('./square');
+
+function sumOfSquares(x, y) {
+  const squareX = square(x);
+  const squareY = square(y);
+
+  return sum(squareX, squareY);
+}
+
+module.exports = sumOfSquares;
