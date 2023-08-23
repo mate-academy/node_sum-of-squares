cddc95db7d79236ba4ca69561d9b2156d4918a87
Oleksandr Kapustei <angelion541@gmail.com>
2023-08-23 11:02:44 +0300

    finish task

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const { sum } = require('./sum');
+const { square } = require('./square');
+
+function sumOfSquares(a, b) {
+  const squareA = square(a);
+  const squareB = square(b);
+
+  return sum(squareA, squareB);
+}
+
+module.exports = { sumOfSquares };
