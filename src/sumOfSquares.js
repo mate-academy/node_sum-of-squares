3258a986e5b6f4de86615aeabda80233f0e9cc2e
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-10-17 14:22:56 +0300

    added modules

--- src/sumOfSquares.js
+++ src/sumOfSquares.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const { sum } = require('./sum');
+const { square } = require('./square');
+
+// eslint-disable-next-line no-unused-vars
+function sumOfSquares(a, b) {
+  return sum(square(a) + square(b));
+}
